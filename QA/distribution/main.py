ae10e5307adc43af811102170f2cc036f698c5a7
Jack Huang <719668276@qq.com>
2022-01-16 12:44:14 +0800

    添加了调试的内容

--- QA/distribution/main.py
+++ QA/distribution/main.py
@@ -3,7 +3,6 @@ from dump_reader import *
 import numpy as np 
 import os 
 # file_path: replace with your own
-#file_path = '100_dump.fus1.lammpstrj'
 file_path = 'dump.recenter.lammpstrj'
 
 # The line number before the first xyz coordinates,
@@ -18,17 +17,17 @@ dump_list= read_dumps(file_path=file_path, extra_line_num=9)
 print('Dump number: {}'.format(len(dump_list)))
 
 # The first dump 
-print(dump_list[0].time_step)
-print(dump_list[0].xyz_df)
+# print(dump_list[0].time_step)
+# print(dump_list[0].xyz_df)
 
-num_bins = 250     #把模拟盒子分成几个小盒子
-num_dump = len(dump_list) #一共有多少帧dump的次数
+num_bins = 250     # 把模拟盒子分成几个小盒子
+num_dump = len(dump_list) # 一共有多少帧 = dump的次数
 HIST = np.empty((num_dump, num_bins)) # 建立一个以行为次数，列为盒子数的一个空的数组，然后把数放进去
 rowNum = 0
 
-volume_bin = 50 * 50 * (250/num_bins)  #单个盒子的体积
-neighbour_num = 10  #以最大粒子数的盒子为基准，隔几个盒子算做浓相范围
-dilute_num = 100    #以盒子的大小为准，确定稀相的范围
+volume_bin = 50 * 50 * (250/num_bins)  # 单个盒子的体积
+neighbour_num = 10  # 以最大粒子数的盒子为基准，隔几个盒子算做浓相范围
+dilute_num = 100    # 以盒子的大小为准，确定稀相的范围
 dense_box = neighbour_num*2
 diluteleft_box = 25
 
@@ -36,30 +35,32 @@ c_de_array = np.empty(num_dump)
 c_di_array = np.empty(num_dump)
 counter = 0 
 
-for dump in dump_list:  #遍历dump list中的所有数
+for dump in dump_list:  # 遍历dump list中的所有数
     # Select all particels
     XYZ = dump.xyz_df
+    print('Before:',XYZ)
     XYZ = XYZ.loc[:, 2:].to_numpy() # Extract x,y,z
-    #print(XYZ)
+    print('After:', XYZ)
 
     X = XYZ[:0]
     # x_c = np.mean(X)  
-
-    hist, bins = np.histogram(X,range=(0, 250), bins=num_bins) #找出x的统计分布，算出每个盒子中的粒子数
+    print(X)
+    hist, bins = np.histogram(X,range=(0, 250), bins=num_bins) # 找出x的统计分布，算出每个盒子中的粒子数
     print('Distribution: {}'.format(hist))
-    idx_max = np.argmax(hist)  #找到每一帧中的最大的粒子数，并且标记它的序号
+    idx_max = np.argmax(hist)  # 找到每一帧中的最大的粒子数，并且标记它的序号
     # print('Index max: {}'.format(idx_max))
+    # 调试: 用input把程序卡住，调试完成后删去
+    input()
+    dense_idxes = np.arange(idx_max-neighbour_num, idx_max+neighbour_num) # 确定浓相的取值范围，根据盒子的大小确定范围
 
-    dense_idxes = np.arange(idx_max-neighbour_num, idx_max+neighbour_num) #确定浓相的取值范围，根据盒子的大小确定范围
-
-    for i in range(len(dense_idxes)):     #判断一下浓相的范围在边界的情况，超过边界的就要循环
+    for i in range(len(dense_idxes)):     # 判断一下浓相的范围在边界的情况，超过边界的就要循环
         if dense_idxes[i] > num_bins - 1:
             dense_idxes[i] = dense_idxes[i] - num_bins
         if dense_idxes[i] < 0:
             dense_idxes[i] = dense_idxes[i] + num_bins
-    # 
+
     # print(dense_idxes)
-    concentration_dense = np.sum(hist[dense_idxes]) / (volume_bin*(dense_box)) #计算浓相的浓度，范围内的粒子数除以范围内的体积
+    concentration_dense = np.sum(hist[dense_idxes]) / (volume_bin*(dense_box)) # 计算浓相的浓度，范围内的粒子数除以范围内的体积
     # print('Concentration dense: {}'.format(concentration_dense))
     
     # input()
@@ -90,22 +91,6 @@ c_de_mean = np.mean(c_de_array)
 c_di_mean = np.mean(c_di_array)
 
 np.savetxt('distribution.txt', HIST, fmt='%3d')
-# np.savetxt('concentration_array.txt', np.vstack((c_de_array, c_di_array)).T, fmt='%.8f')
-# np.savetxt('concentration_mean.txt', np.array([c_de_mean*1661, c_di_mean*1661,c_de_mean/c_di_mean]), fmt='%.8f')
-# 
-# for i in HIST:
-#     dense=np.sum(hist[7:17])
-#     dilute=np.sum(hist[0:5]) + np.sum(hist[-5:])
-#     c_de=dense/50000
-#     c_di=dilute/125000
-     
-
-#c_de_mean = np.mean(c_de_array)
-#c_di_mean = np.mean(c_di_array)
-
-#np.savetxt('concentration_array.txt', np.vstack((c_de_array, c_di_array)).T, fmt='%.4f')
-#np.savetxt('concentration_mean.txt', np.array([c_de_mean, c_di_mean]), fmt='%.4f')
-
 
 
 
